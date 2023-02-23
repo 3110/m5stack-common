6bd705327b936dc07b51895328400b24ca9abe58
SAITO, Tetsuya <saito.tetsuya@gmail.com>
2023-02-23 19:05:31 +0900

    ディレクトリ構造の変更

--- include/Debug.h
+++ include/Debug.h
@@ -6,18 +6,18 @@
 
 #ifdef ENABLE_SERIAL_MONITOR
 #include <Arduino.h>
-#define SERIAL_BEGIN(a) Serial.begin(a)
-#define SERIAL_PRINT(a) Serial.print(a)
-#define SERIAL_PRINTLN(a) Serial.println(a)
+#define SERIAL_BEGIN(a)    Serial.begin(a)
+#define SERIAL_PRINT(a)    Serial.print(a)
+#define SERIAL_PRINTLN(a)  Serial.println(a)
 #define SERIAL_PRINTF(...) Serial.printf(__VA_ARGS__)
 #define SERIAL_PRINTF_LN(...)       \
     {                               \
         Serial.printf(__VA_ARGS__); \
         Serial.println();           \
     }
-#define SERIAL_MAC_ADDRESS_PRINT(a) printMacAddress(a, false)
+#define SERIAL_MAC_ADDRESS_PRINT(a)   printMacAddress(a, false)
 #define SERIAL_MAC_ADDRESS_PRINTLN(a) printMacAddress(a, true)
-#define SERIAL_DUMP(d, l) dump(d, l)
+#define SERIAL_DUMP(d, l)             dump(d, l)
 extern void printMacAddress(const uint8_t* macAddr, const bool newline);
 extern void dump(const uint8_t* data, size_t len, uint8_t indent = 2,
                  uint8_t width = 16);
